6bc075d3664eec3fa17ee6696428ded4a23c7cbd
Maciej Ziaja <info@lu-tech.pl>
2017-10-05 22:24:32 +0200

    manifest and offline plugins setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,17 @@ module.exports = {
         trackingId: 'UA-21681130-1',
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Lu-Tech",
+        short_name: "Lu-Tech",
+        start_url: "/",
+        background_color: "#F38630",
+        theme_color: "#A7DBD8",
+        display: "minimal-ui",
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
-}
+};
