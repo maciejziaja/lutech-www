afdc75634d7375add8dcb23077c67c2da6a7cf8c
Maciej Ziaja <info@lu-tech.pl>
2017-10-06 06:22:38 +0200

    social icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import styles from './index.module.css';
+import config from '../../config/config';
+import Social from '../components/Social';
+
+const socials = config.socials.map(social => <Social social={social} key={social.name} />);
 
 const IndexPage = () => (
   <div>
-    <h1>Lu-Tech</h1>
-    <p>Hey guys, I'm Maciej Ziaja.</p>
-    <p>I'm software developer</p>
+    <h1 className={styles.title}>Lu-Tech</h1>
+    <p className={styles.text}>Hey guys, I'm Maciej Ziaja.</p>
+    <p className={styles.text}>I'm software developer</p>
+    <div className={styles.socials}>
+      { socials }
+    </div>
   </div>
 );
 
